4c4d9c1c21897cd80812efcaa0f67c0bce40650d
Roskils <115069492+Roskils@users.noreply.github.com>
2024-11-08 05:32:25 +0300

    ssqq

--- HotelManagementSystem/src/App.java
+++ HotelManagementSystem/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1231231\n");
+        System.out.println("qhdslfjsa");
         
     }
 }
