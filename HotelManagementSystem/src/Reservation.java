7793a6c1e6df1e8f9d0143da291c8ee2d70906da
Ali <Ali_adnan2015@hotmail.com>
2024-11-30 12:22:37 +0000

    oeiroegroegrioerg

--- HotelManagementSystem/src/Reservation.java
+++ HotelManagementSystem/src/Reservation.java
@@ -1,10 +1,24 @@
-public class Reservation {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+public class Reservation {
+    //date code 
+    int year,month,day;
+    int year2,month2,day2;
+    LocalDate checkIn = LocalDate.of(year,month,day);
+    LocalDate checkOut = LocalDate.of(year2,month2,day2);
+    private double DaysInRoom = ChronoUnit.DAYS.between(checkIn,checkOut);
     private int reservationId;
     private double totalPrice;
     
+    
+
+    double calculateTotalPrice()
+    {
 
 
+        return 0;
+    }
     public Reservation(){
         
     }
